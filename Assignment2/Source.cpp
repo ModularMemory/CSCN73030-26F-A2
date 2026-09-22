f00947352e1da5edc832ba28037f9ceb37fd96b4
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2026-09-22 19:50:42 -0400

    Read student file and write parsed data to screen

---
+++ Assignment2/Source.cpp
@@ -0,0 +1,56 @@
+#include "student_data.h"
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <optional>
+#include <vector>
+
+using namespace std;
+
+optional<STUDENT_DATA> readStudent(istream& stream) {
+    string line;
+    getline(stream, line);
+
+    if (line.empty()) {
+        return {};
+    }
+
+    istringstream lineStream(line);
+
+    string firstName;
+    string lastName;
+    getline(lineStream, firstName, ',');
+    getline(lineStream, lastName, ',');
+
+    return STUDENT_DATA { .firstName = firstName, .lastName = lastName };
+}
+
+int main(int argc, char* argv[]) {
+    const string filename = "StudentData.txt";
+
+    ifstream fin;
+    fin.open(filename);
+    if (!fin.is_open()) {
+        cout << "Failed to open file: " << filename << endl;
+        return 1;
+    }
+
+    vector<STUDENT_DATA> students;
+    while (true) {
+        optional<STUDENT_DATA> student = readStudent(fin);
+        if (student.has_value()) {
+            students.push_back(student.value());
+        }
+
+        if (fin.eof()) {
+            break;
+        }
+    }
+
+    for (const auto& student : students) {
+        cout << "Student: " << student.firstName << ", " << student.lastName << endl;
+    }
+
+    return 0;
+}
